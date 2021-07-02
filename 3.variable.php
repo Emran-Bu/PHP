0320f7a82cb89b0d7122d358a8bbd262a30c89f1
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-02 07:48:40 +0600

    variable

---
+++ 3.variable.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = "Emran <br>";
+
+echo $a;
+echo $a;
+echo $a;
+echo $a;
+
+$a = "Hello";
+$b = "World";
+
+$d = "Emarn " . "hasan <br>";
+
+
+
+echo $d;
+
+$c = $a . $b;
+
+echo "How are you : " . $c . '<br>';
+echo $c;
+
+echo  "H " , "hasan";
+?>
