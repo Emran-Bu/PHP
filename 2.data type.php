fa914b79ee144045baf4f7b66cae732f9de04d3c
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-02 07:48:26 +0600

    data type

---
+++ 2.data type.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = "Emran" . "<br>";
+var_dump($a);
+
+$b = "2025"  . "<br>";
+var_dump($b);
+
+$c = 2025;
+var_dump($c);
+
+$c = 2025.50;
+var_dump($c);
+
+$c = true;
+var_dump($c);
+
+$c = Null;
+var_dump($c);
+
+$x = array("html", "css", "php");
+echo $x[0] ."<br>";
+var_dump($x);
+
+?>
