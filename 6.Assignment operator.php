e88abcc3d1e2f5165823d950080ef2660ee7889b
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-03 09:55:10 +0600

    Assignment operator

---
+++ 6.Assignment operator.php
@@ -0,0 +1,46 @@
+<?php
+
+//addition
+$num1 = 5;
+$num2 = 10;
+
+$num1 += $num2;
+echo "the addition is : " . $num1 . "<br>";
+
+//subtraction
+$num1 = 15;
+$num2 = 10;
+
+$num1 -= $num2;
+echo "the subtraction is : " . $num1. "<br>";
+
+//multiplication
+$num1 = 5;
+$num2 = 10;
+
+$num1 *= $num2;
+echo "the multiplication is : " . $num1. "<br>";
+
+//Divided
+$num1 = 50;
+$num2 = 5;
+
+$num1 /= $num2;
+echo "the Divided is : " . $num1. "<br>";
+
+//Modulus
+$num1 = 50;
+$num2 = 6;
+
+$num1 %= $num2;
+echo "the Modulus is : " . $num1. "<br>";
+
+//Exponentiation
+$num1 = 2;
+$num2 = 3;
+
+$num1 **= $num2;
+echo "the Exponentiation is : " . $num1. "<br>";
+
+
+?>
