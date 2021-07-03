6d22e75a861387b34fafacf385fb3012dfc69ade
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-03 09:56:04 +0600

    if statement

---
+++ 8.if statement.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = 5;
+$b = 10;
+
+if($a > $b):
+
+echo "a is greater than b <br>";
+
+endif;
+
+echo "a is not greater than b <br>";
+
+$c = 20;
+
+if($c > 10){
+    echo "c is greater than 10 <br>";
+}
+
+echo "a is not greater than b <br>";
+
+?>
