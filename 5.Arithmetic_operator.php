8431595a12c7c80f8977c88cfcaff42ae86d50fa
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-03 09:54:43 +0600

    Arithmetic_operator

---
+++ 5.Arithmetic_operator.php
@@ -0,0 +1,57 @@
+<?php
+
+//addition
+$num1 = 5;
+$num2 = 10;
+
+$result = $num1 + $num2;
+echo "the addition is : " . $result . "<br>";
+
+//subtraction
+$num1 = 15;
+$num2 = 10;
+
+$result = $num1 - $num2;
+echo "the subtraction is : " . $result. "<br>";
+
+//multiplication
+$num1 = 5;
+$num2 = 10;
+
+$result = $num1 * $num2;
+echo "the multiplication is : " . $result. "<br>";
+
+//Divided
+$num1 = 50;
+$num2 = 5;
+
+$result = $num1 / $num2;
+echo "the Divided is : " . $result. "<br>";
+
+//Modulus
+$num1 = 50;
+$num2 = 6;
+
+$result = $num1 % $num2;
+echo "the Modulus is : " . $result. "<br>";
+
+//Exponentiation
+$num1 = 2;
+$num2 = 3;
+
+$result = $num1 ** $num2;
+echo "the Exponentiation is : " . $result. "<br>";
+
+//Increment
+$num1 = 2;
+
+$result = ++$num1;
+echo "the Increment is : " . $result. "<br>";
+
+//Decrement
+$num1 = 2;
+
+$result = --$num1;
+echo "the Decrement is : " . $result. "<br>";
+
+?>
