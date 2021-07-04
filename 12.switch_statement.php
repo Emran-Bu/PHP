79b0b8ae88cde69455d6b2b391459fb171523240
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-04 17:21:32 +0600

    switch_statement

---
+++ 12.switch_statement.php
@@ -0,0 +1,68 @@
+<?php
+
+    $day = 15;
+    switch($day){
+
+        case 1:
+            echo "saturday";
+            break;
+        case 2:
+            echo "sunday";
+            break;
+
+        case 3:
+            echo "monday";
+            break;
+        case 4:
+            echo "tuesday";
+            break;
+
+        case 5:
+            echo "wednesday";
+            break;
+        case 6:
+            echo "thursday";
+            break;
+        case 7:
+            echo "friday";
+            break;
+
+        default:
+            echo "invalid";
+    }
+?>
+<hr>
+<?php
+
+    $day = 4;
+    switch($day):
+
+        case 1:
+            echo "saturday";
+            break;
+        case 2:
+            echo "sunday";
+            break;
+
+        case 3:
+            echo "monday";
+            break;
+        case 4:
+            echo "tuesday";
+            break;
+
+        case 5:
+            echo "wednesday";
+            break;
+        case 6:
+            echo "thursday";
+            break;
+        case 7:
+            echo "friday";
+            break;
+
+        default:
+            echo "invalid";
+        endswitch;
+
+?>
