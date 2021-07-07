8fb8e131e2b20321e94f2ee2a2431ff6dcb50647
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-07 08:20:00 +0600

    function

---
+++ 21.function.php
@@ -0,0 +1,22 @@
+<?php
+
+    function hello()
+    {
+        echo "Hello Everybody<br>";
+    }
+
+    hello();
+    echo "example<br>";
+    hello();
+
+    function welcome(){
+        echo "welcome Everybody<br>";
+    }
+
+    welcome();
+    echo "example2<br>";
+    welcome();
+    welcome();
+    welcome();
+
+?>
