c3881a12f0432b46874a60e3925228d38e74d8b8
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-07 08:22:05 +0600

    variable funtion

---
+++ 25.variable function.php
@@ -0,0 +1,17 @@
+<?php
+
+    $hello = function ($name){
+        echo "Hello $name" . "<br>";
+    };
+
+    $hello("World");
+
+    function world($nam){
+        echo "Hello $nam";
+    }
+
+    $fun = "world";
+    $fun("Bangladesh");
+
+
+?>
