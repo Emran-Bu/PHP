63c8c8a42dac14d42e1b153a52152b82a4c8d609
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-16 07:59:22 +0600

    array list function

---
+++ 57.array list function.php
@@ -0,0 +1,11 @@
+<?php
+
+$color = array('red', 'blue', 'green');
+
+    list($a, $b, $c) = $color;
+
+    echo "value of a : $a <br>";
+    echo "value of a : $b <br>";
+    echo "value of a : $c <br>";
+
+?>
