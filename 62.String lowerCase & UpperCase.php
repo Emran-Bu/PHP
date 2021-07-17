391d32e5933e81c446bb5c1367bac41ca7e5e4cc
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-17 19:45:31 +0600

    String lowerCase & UpperCase

---
+++ 62.String lowerCase & UpperCase.php
@@ -0,0 +1,41 @@
+<?php
+
+    $str = "hello world";
+
+    $arr = strtoupper($str);
+
+    echo $arr;
+
+    echo "<br>";
+
+    $str = "hello world";
+
+    $arr = strtolower($str);
+
+    echo $arr;
+
+    echo "<br>";
+
+    $str = "hello world";
+
+    $arr = ucfirst($str);
+
+    echo $arr;
+
+    echo "<br>";
+
+    $str = "hello world";
+
+    $arr = lcfirst($str);
+
+    echo $arr;
+
+    echo "<br>";
+
+    $str = "hello world";
+
+    $arr = ucwords($str);
+
+    echo $arr;
+
+?>
