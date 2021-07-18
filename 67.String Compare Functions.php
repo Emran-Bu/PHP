f8be4f1d5f858f0c2f12572424eae84fccb43337
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-18 06:12:04 +0600

    String Compare Functions

---
+++ 67.String Compare Functions.php
@@ -0,0 +1,9 @@
+<?php
+
+    echo strcmp("Hello World!", "Hello World!");
+    echo "<br>";
+    echo strcmp("Hello World!", "Hello World! world");
+    echo "<br>";
+    echo strcmp("Hello World!", "Hello");
+
+?>
