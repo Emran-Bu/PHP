331892338788f56201181df21055f9ae44d315f1
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-18 06:11:11 +0600

    str substr

---
+++ 65.str substr.php
@@ -0,0 +1,13 @@
+<?php
+
+    $str = "i love php, i love php tool";
+
+    echo substr($str, 2, 15);
+
+    echo "<br>";
+
+    $str = "i love php, i love php tool";
+
+    echo substr($str, -5);
+
+?>
