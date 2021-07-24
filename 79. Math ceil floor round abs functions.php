69f454ff694ff423b5209dc094062b15216744fb
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-24 22:20:04 +0600

    Math ceil floor round abs functions

---
+++ 79. Math ceil floor round abs functions.php
@@ -0,0 +1,52 @@
+<?php
+
+    echo "Math ceil functions<br> <br>";
+    echo"ceil no 0.1 of : " . (ceil(0.1))."<br>";
+    echo"ceil no 0.45 of : " . (ceil(0.45))."<br>";
+    echo"ceil no 0.55 of : " . (ceil(0.55))."<br>";
+    echo"ceil no 0.80 of : " . (ceil(0.80))."<br>";
+    echo"ceil no 0.99 of : " . (ceil(0.99))."<br>";
+    echo"ceil no 1.1 of : " . (ceil(1.1))."<br>";
+    echo"ceil no 1.5 of : " . (ceil(1.5))."<br>";
+    echo"ceil no 1.9 of : " . (ceil(1.9))."<br>";
+    echo"ceil no -2.6 of : " . (ceil(-2.6))."<br>";
+
+    echo "<br>Math floor functions<br> <br>";
+    echo"floor no 0.1 of : " . (floor(0.1))."<br>";
+    echo"floor no 0.45 of : " . (floor(0.45))."<br>";
+    echo"floor no 0.55 of : " . (floor(0.55))."<br>";
+    echo"floor no 0.80 of : " . (floor(0.80))."<br>";
+    echo"floor no 0.99 of : " . (floor(0.99))."<br>";
+    echo"floor no 1.1 of : " . (floor(1.1))."<br>";
+    echo"floor no 1.5 of : " . (floor(1.5))."<br>";
+    echo"floor no 1.9 of : " . (floor(1.9))."<br>";
+    echo"floor no 2.6 of : " . (floor(2.6))."<br>";
+    echo"floor no -4.6 of : " . (floor(-4.6))."<br>";
+
+    echo "<br>Math round functions<br> <br>";
+    echo"round no 0.1 of : " . (round(0.1))."<br>";
+    echo"round no 0.45 of : " . (round(0.45))."<br>";
+    echo"round no 0.55 of : " . (round(0.55))."<br>";
+    echo"round no 0.80 of : " . (round(0.80))."<br>";
+    echo"round no 0.99 of : " . (round(0.99))."<br>";
+    echo"round no 1.1 of : " . (round(1.1))."<br>";
+    echo"round no 1.5 of : " . (round(1.5))."<br>";
+    echo"round no 1.9 of : " . (round(1.9))."<br>";
+    echo"round no 2.6 of : " . (round(2.6))."<br>";
+    echo"round no -2.6 of : " . (round(-4.4))."<br>";
+    echo"round no -4.6 of : " . (round(-4.6))."<br>";
+
+    echo "<br>Math abs functions<br> <br>";
+    echo"abs no 0.1 of : " . (abs(0.1))."<br>";
+    echo"abs no 0.45 of : " . (abs(0.45))."<br>";
+    echo"abs no 0.55 of : " . (abs(0.55))."<br>";
+    echo"abs no 0.80 of : " . (abs(0.80))."<br>";
+    echo"abs no 0.99 of : " . (abs(0.99))."<br>";
+    echo"abs no 1.1 of : " . (abs(1.1))."<br>";
+    echo"abs no 1.5 of : " . (abs(1.5))."<br>";
+    echo"abs no 1.9 of : " . (abs(1.9))."<br>";
+    echo"abs no 2.6 of : " . (abs(2.6))."<br>";
+    echo"abs no -2.6 of : " . (abs(-4.4))."<br>";
+    echo"abs no -4.6 of : " . (abs(-4.6))."<br>";
+
+?>
