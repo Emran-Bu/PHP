57b6a96d7afafae5c50ad1d78fe34421c69c254d
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-25 07:06:53 +0600

    Math Intdiv sqrt pow functions

---
+++ 80.Math Intdiv sqrt pow functions.php
@@ -0,0 +1,40 @@
+<?php
+
+    echo "Math intdiv functions<br> <br>";
+    echo"intdiv no (8, 4) of : " . (intdiv(8, 4))."<br>";
+    echo"intdiv no (0.45, 2) of : " . (intdiv(0.45, 2))."<br>";
+    echo"intdiv no (0.55, 4) of : " . (intdiv(0.55, 4))."<br>";
+    echo"intdiv no (5, 2) of : " . (intdiv(5, 2))."<br>";
+    echo"intdiv no (10, 3) of : " . (intdiv(10, 3))."<br>";
+    echo"intdiv no (-5, -2) of : " . (intdiv(-5, -2))."<br>";
+    echo"intdiv no (-5, 2) of : " . (intdiv(-5, 2))."<br>";
+    echo"intdiv no (5, -2) of : " . (intdiv(5, -2))."<br>";
+
+    echo "<br>Math sqrt functions<br> <br>";
+    echo"sqrt no 0 of : " . (sqrt(0))."<br>";
+    echo"sqrt no 0.1 of : " . (sqrt(0.1))."<br>";
+    echo"sqrt no 0.45 of : " . (sqrt(0.45))."<br>";
+    echo"sqrt no 1.1 of : " . (sqrt(1.1))."<br>";
+    echo"sqrt no 1.5 of : " . (sqrt(1.5))."<br>";
+    echo"sqrt no 2 of : " . (sqrt(2))."<br>";
+    echo"sqrt no 6 of : " . (sqrt(6))."<br>";
+    echo"sqrt no 5 of : " . (sqrt(5))."<br>";
+    echo"sqrt no 1010 of : " . (sqrt(1010))."<br>";
+    echo"sqrt no 11 of : " . (sqrt(11))."<br>";
+    echo"sqrt no 12 of : " . (sqrt(12))."<br>";
+    echo"sqrt no 9 of : " . (sqrt(9))."<br>";
+    echo"sqrt no 16 of : " . (sqrt(16))."<br>";
+
+    echo "<br>Math pow functions<br> <br>";
+    echo"pow no (0.1, 2) of : " . (pow(0.1, 2))."<br>";
+    echo"pow no (0.1, 3) of : " . (pow(0.1, 3))."<br>";
+    echo"pow no (0.55, 4) of : " . (pow(0.55, 4))."<br>";
+    echo"pow no (1.1, 2) of : " . (pow(1.1, 2))."<br>";
+    echo"pow no (5, 2) of : " . (pow(5, 2))."<br>";
+    echo"pow no (5, 3) of : " . (pow(5, 3))."<br>";
+    echo"pow no (5, 4) of : " . (pow(5, 4))."<br>";
+    echo"pow no (5, 5) of : " . (pow(5, 5))."<br>";
+    echo"pow no (-2, 4) of : " . (pow(-2, 4))."<br>";
+    echo"pow no (-2, -4) of : " . (pow(-2, -4))."<br>";
+
+?>
