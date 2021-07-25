6265cdead5815785c7c7ac9f3a4d9c9a2a41cfdd
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-07-25 07:07:31 +0600

    Math Rand, Mt_rand & Lcg_valu

---
+++ 82.Date functions.php
@@ -0,0 +1,32 @@
+<?php
+
+    echo "Today full date is : " . date("d/m/y") . "<br>";
+    echo "<br>";
+    echo "Today full date is : " . date("d-M-Y") . "<br>";
+    echo "<br>";
+    echo "Today full date is : " . date("d/F/Y") . "<br>";
+    echo "<br>";
+
+    echo "Today is d : " . date("d") . "<br>";
+    echo "Today is j : " . date('j') . "<br>";
+    echo "Today is dS : " . date('dS') . "<br>";
+    echo "Today is jS : " . date('jS') . "<br>";
+    echo "<br>";
+    echo "Month is F : " . date("F") . "<br>";
+    echo "Month is M : " . date('M') . "<br>";
+    echo "Month is m : " . date('m') . "<br>";
+    echo "Month is n: " . date('n') . "<br>";
+    echo "<br>";
+    echo "Month is Y : " . date("Y") . "<br>";
+    echo "Month is y : " . date('y') . "<br>";
+    echo "<br>";
+    echo "week day is D : " . date("D") . "<br>";
+    echo "week day is l : " . date("l") . "<br>";
+    echo "week day is N : " . date("N") . "<br>";
+    echo "week day is w : " . date('w') . "<br>";
+    echo "day is z : " . date('z') . "<br>";
+    echo "Week of the year W: " . date('W') . "<br>";
+    echo "date of the month t: " . date('t') . "<br>";
+    echo "this year is leap year 1 or not leap year 0: " . date('L') . "<br>";
+
+?>
