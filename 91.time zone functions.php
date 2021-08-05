fef608c6da8790639110ce3256ae378667218e74
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-08-05 20:46:19 +0600

    time zone functions

---
+++ 91.time zone functions.php
@@ -0,0 +1,17 @@
+<?php
+
+    date_default_timezone_set("Asia/Dhaka");
+    echo date_default_timezone_get() . "<br>";
+
+    $tz = timezone_open("Asia/Dhaka");
+    echo timezone_name_get($tz);
+
+    echo "<pre>";
+    print_r(timezone_location_get($tz));
+    echo "</pre>";
+
+    echo "<br>timezone_identifiers_list<br>";
+    echo "<pre>";
+    print_r(timezone_identifiers_list());
+    echo "</pre>";
+?>
