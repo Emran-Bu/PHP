72b9cf4ae111b12cc2e9f821c9ccbbd344beebd2
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-08-06 07:34:44 +0600

    index

---
+++ 92.PHP Include & Require Statement/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>www.bd20.com</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="wrapper">
+<?php include 'header.php';  ?>
+<?php require 'header.php';  ?>
+	<nav>
+		<div id="nav">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Insert</a></li>
+				<li><a href="#">Item &raquo;</a>
+					<ul>
+						<li><a href="#">Html</a></li>
+						<li><a href="#">Css</a></li>
+						<li><a href="#">Java &raquo;</a>
+							<ul>
+								<li><a href="#">Wordpress</a></li>
+								<li><a href="#">Bootstrap</a></li>
+								<li><a href="#">Python&raquo;</a>
+									<ul>
+										<li><a href="#">Html</a></li>
+										<li><a href="#">Css</a></li>
+										<li><a href="#">Java</a></li>
+										<li><a href="#">Script</a></li>
+										<li><a href="#">Query</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Php</a></li>
+								<li><a href="#">C++</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Script</a></li>
+						<li><a href="#">Query</a></li>
+					</ul>
+
+				</li>
+				<li><a href="#">Select</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#">Menu&raquo;</a>
+					<ul>
+						<li><a href="#">Html</a></li>
+						<li><a href="#">Css</a></li>
+						<li><a href="#">Java&raquo;</a>
+
+							<ul>
+								<li><a href="#">Wordpress</a></li>
+								<li><a href="#">Bootstrap</a></li>
+								<li><a href="#">Python&raquo;</a>
+									<ul>
+										<li><a href="#">Html</a></li>
+										<li><a href="#">Css</a></li>
+										<li><a href="#">Java</a></li>
+										<li><a href="#">Script</a></li>
+										<li><a href="#">Query</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Php</a></li>
+								<li><a href="#">C++</a></li>
+							</ul>
+
+						</li>
+						<li><a href="#">Script</a></li>
+						<li><a href="#">Query</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Remarks</a></li>
+			</ul>
+		</div>
+	</nav> 
+	<section>
+		<div id="slider">
+			<img src="img/flower.JPG" alt="Flower Image">
+		</div>
+	</section>
+	<div id="content_wrapper">
+		<div id="content">
+			<article>
+				<h2>Loren ipsum site dolar amet consecttetuer</h2>
+				<p>The industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged electronic typesetting, remaining essentially unchanged electronic typesetting remaining survivedi</p>
+				
+			</article>
+		</div>
+		<div id="sidebar">
+			<aside>
+				<h2>Sidebar Heading</h2>
+				<ul>
+					<li><a href="#">Loren ipsum site dola</a></li>
+					<li><a href="#">Loren ipsum site dola</a></li>
+					<li><a href="#">Loren ipsum site dola</a></li>
+					<li><a href="#">Loren ipsum site dola</a></li>
+					<li><a href="#">Loren ipsum site dola</a></li>
+					<li><a href="#">Loren ipsum site dola</a></li>
+				</ul>
+			</aside>
+		</div>	
+	</div>
+	<?php 
+	include 'footer.php' 
+	?>
+	<?php 
+		require 'footer.php' 
+	?>
+	<?php include 'footer.php' ?>
+</div>
+</body>
+</html>
