7749e920f77699e9a7983d71994c84f012d580e8
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-08-10 08:48:09 +0600

    ,  &

---
+++ 94.$_GET, $_POST & $_REQUEST/testform.php
@@ -0,0 +1,24 @@
+<?php
+
+    // echo "<pre>";
+    // print_r($_GET);
+    // echo "<pre>";
+
+    // echo $_GET['fname'] . "<br>";
+    // echo $_GET['age'] . "<br>";
+
+    // echo "<pre>";
+    // print_r($_POST);
+    // echo "<pre>";
+
+    // echo $_POST['fname'] . "<br>";
+    // echo $_POST['age'] . "<br>";
+
+    echo "<pre>";
+    print_r($_REQUEST);
+    echo "<pre>";
+
+    echo $_REQUEST['fname'] . "<br>";
+    echo $_REQUEST['age'] . "<br>";
+
+?>
