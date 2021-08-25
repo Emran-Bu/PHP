b5dae4362fd58bca796a8c06650e1535efd4c146
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-08-25 07:17:55 +0600

    odd number print using for loop

---
+++ basicPhpDarunIT/5.odd number print using for loop.php
@@ -0,0 +1,21 @@
+<?php
+
+    for ($i = 1; $i <= 30; $i++) { 
+        if ($i % 2 == 0) {
+            echo $i . "<br>";
+        }
+    }
+
+    echo "<br>";
+
+    for ($i = 1; $i <= 30; $i++) { 
+        if ($i % 2 == 1) {
+            echo $i . "<br>";
+        }
+    }
+
+    for ($i=1; $i <=10 ; $i++) { 
+        echo "$i. Emran <br>";
+    }
+
+?>
