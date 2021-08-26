375a8ad62dbbc77442115d90384b96683590b5e0
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-08-26 07:38:08 +0600

    functiom

---
+++ basicPhpDarunIT/10.function.php
@@ -0,0 +1,19 @@
+<?php
+
+    function para($name, $age){
+        echo "Hello! This is mr. $name. I am $age years old.";
+    }
+
+    para('Emran Hasan', 22);
+
+?>
+
+<?php
+
+    function add($x, $y){
+        return $x + $y;
+    }
+
+    echo "<br>addition is: " . add(22, 22);
+
+?>
