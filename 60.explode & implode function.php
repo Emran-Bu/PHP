3b6f7c82f6e3e54d650887588478cc676c87d0b1
Freelancer-Emran <mdemranhasan809@gmail.com>
2022-11-09 23:54:04 +0600

    str

--- 60.explode & implode function.php
+++ 60.explode & implode function.php
@@ -20,4 +20,21 @@
     echo "<pre>";
     print_r($array);
     echo "<pre>";
+
+    $sr = "Hello World.jpg";
+     list($fi) = explode('.' , $sr);
+     list($_, $se) = explode('.' , $sr);
+     $first = current(explode('.' , $sr));
+     $second = end(explode('.' , $sr));
+     echo $first . "<br>";
+     echo $second . "<br>";
+     echo $se . "<br>";
+     echo $fi . "<br>";
+
+     echo "<br>";
+
+    $str = "Hi/I £love_php 41563456450 00000, -and !i 124love: php tool";
+    echo preg_replace('/[^A-Za-z0-9\-]/', '_', $str);
+    echo "<br>";
+
 ?>
